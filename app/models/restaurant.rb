37ede2dfbc0bb3043c553514854bcd12fbb4e9aa
Vincent Ramos <vince9322@gmail.com>
2022-05-18 16:22:20 +0200

    Exercise 2

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,5 +4,4 @@ class Restaurant < ApplicationRecord
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :category, inclusion: {in: CATEGORIES}
   has_many :reviews, dependent: :destroy
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+Restaurant.destroy_all
+
+50.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
